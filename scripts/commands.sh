8cc7e801ede4b036622cc714e36d996319ae8660
Arthur Gomes Alves <desenvolvimentonit@gmail.com>
2024-09-09 12:26:16 -0300

    remove commands.sh

--- scripts/commands.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# O shell irá encerrar a execução do script quando um comando falhar
-set -e
-
-python manage.py collectstatic --noinput
-python manage.py migrate --noinput
-python manage.py runserver 0.0.0.0:8000
