455e12d050d81f5cc6fcf161112335f845648a0e
Arth-23 <arthuralves@ads.fiponline.edu.br>
2025-01-27 16:54:52 -0300

    Tela de login concluida

--- code/project/forms.py
+++ code/project/forms.py
@@ -5,5 +5,5 @@ class LoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super(LoginForm, self).__init__(*args, **kwargs)
         
-        self.fields['username'].widget.attrs['placeholder'] = 'Nome De Usuário:'
-        self.fields['password'].widget.attrs['placeholder'] = 'Senha:'
+        self.fields['username'].widget.attrs['placeholder'] = 'Nome de Usuário'
+        self.fields['password'].widget.attrs['placeholder'] = 'Senha'
